5252df34142305fef740f68849ac261b4433042b
pauloaragao <paulo.silva@dcomp.ufs.br>
2023-05-14 15:52:05 -0300

    feat: new testers

---
+++ src/category/tests/domain/test_entities.py
@@ -0,0 +1,10 @@
+import unittest
+from datetime import datetime
+from category.domain.entities import Category
+
+class TestCategory(unittest.TestCase):
+
+    def test_constructor(self):
+        category = Category('Movie', 'some description', True, datetime.now())
+        self.assertEqual(category.name, 'Movie')
+        
